373ef703fd819686ce6e05643b5504cd29ae1fc8
mschickler <mschickler@users.noreply.github.com>
2026-02-21 10:07:25 -0500

    Upgrade bike_miles.py to Python 3 and latest Strava API (#2)

--- bike_miles.py
+++ bike_miles.py
@@ -25,119 +25,121 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 '''
 ###############################################################################
 
-#!/usr/bin/python
+#!/usr/bin/env python3
 
 import sys
 import time
-import requests
 import json
 
+try:
+  import requests
+except ImportError:
+  print("This script requires the requests library. Please install it using:")
+  print("  pip install requests")
+  sys.exit(1)
+
+STRAVA_API_BASE = "https://www.strava.com/api/v3"
+
 def print_token_help():
-  print "To learn more about acquiring an access token, go to:"
-  print "  http://strava.github.io/api/#access"
+  print("To learn more about acquiring an access token, go to:")
+  print("  https://developers.strava.com/docs/authentication/")
 
-# Check arguments
-if len(sys.argv) < 3:
-  print ""
-  print "You must specify the year and a valid access token."
-  print ""
-  print_token_help()
-  print ""
-  print "Usage: " + sys.argv[0] + " <access-token> <year>"
-  print ""
-  exit()
-
-# The access token is a required argument
-access_token = sys.argv[1]
-
-# The year is a required argument.  Calculate the epoch starting
-# time and ending time for the specified year.
-year = int(sys.argv[2])
-pattern = "%d.%m.%Y"
-date_time = "01.01." + str(year)
-start_epoch = int(time.mktime(time.strptime(date_time, pattern)))
-date_time = "01.01." + str(year+1)
-end_epoch = int(time.mktime(time.strptime(date_time, pattern)))
-
-# Query the athlete data to grab the bike IDs and names
-print ""
-print "Retrieving athlete information..."
-print ""
-result = requests.get(
-  "https://www.strava.com/api/v3/athlete",
-  headers = {
-    "Authorization" : "Bearer " + access_token
-  }
-)
-athlete = json.loads(result.text)
-
-# For this first query, check to make sure we are authorized
-if "message" in athlete:
-  if athlete["message"] == "Authorization Error":
-    print ""
-    print "Access token is invalid."
-    print ""
+def main():
+  # Check arguments
+  if len(sys.argv) < 3:
+    print("\nYou must specify the year and a valid access token.\n")
     print_token_help()
-    print ""
+    print(f"\nUsage: {sys.argv[0]} <access-token> <year>\n")
     exit()
 
-# Query the activities.  There is a limit to the number of activities
-# that will be returned at one time, so keep querying until we've
-# received all of the pages.
-page = 1
-all_activities = []
-while True:
-  print "Retrieving page " + str(page) + " of activities..."
+  # The access token is a required argument
+  access_token = sys.argv[1]
+
+  # The year is a required argument.  Calculate the epoch starting
+  # time and ending time for the specified year.
+  year = int(sys.argv[2])
+  pattern = "%d.%m.%Y"
+  date_time = f"01.01.{year}"
+  start_epoch = int(time.mktime(time.strptime(date_time, pattern)))
+  date_time = f"01.01.{year+1}"
+  end_epoch = int(time.mktime(time.strptime(date_time, pattern)))
+
+  # Query the athlete data to grab the bike IDs and names
+  print("\nRetrieving athlete information...\n")
   result = requests.get(
-    "https://www.strava.com/api/v3/athlete/activities",
-    params = {
-      "page" : page,
-      "after" : start_epoch,
-      "before" : end_epoch,
-      "per_page" : 200
-    },
+    f"{STRAVA_API_BASE}/athlete",
     headers = {
       "Authorization" : "Bearer " + access_token
-    }
+    },
+    timeout=10
   )
-  activities = json.loads(result.text)
-  if len(activities) > 0:
-    all_activities += activities
-  else:
-    break
-  page += 1
+  athlete = result.json()
 
-print ""
-print "Retrieved " + str(len(all_activities)) + " total activities."
+  # For this first query, check to make sure we are authorized
+  if "message" in athlete:
+    if athlete["message"] == "Authorization Error":
+      print("\nAccess token is invalid.\n")
+      print_token_help()
+      print("")
+      exit()
 
-# Build a lookup table that maps bike IDs to bike names
-longest_bike_name = 0
-bike_name = {}
-for bike in athlete["bikes"]:
-  bike_name[bike["id"]] = bike["name"]
-  if len(bike["name"]) > longest_bike_name:
-    longest_bike_name = len(bike["name"])
+  # Query the activities.  There is a limit to the number of activities
+  # that will be returned at one time, so keep querying until we've
+  # received all of the pages.
+  page = 1
+  all_activities = []
+  while True:
+    print(f"Retrieving page {page} of activities...")
+    result = requests.get(
+      f"{STRAVA_API_BASE}/athlete/activities",
+      params = {
+        "page" : page,
+        "after" : start_epoch,
+        "before" : end_epoch,
+        "per_page" : 200
+      },
+      headers = {
+        "Authorization" : "Bearer " + access_token
+      },
+      timeout=10
+    )
+    activities = result.json()
+    if len(activities) > 0:
+      all_activities += activities
+    else:
+      break
+    page += 1
 
-# Count the number of miles traveled on each bike
-bike_distance = {}
-for activity in all_activities:
-  bike_id = activity["gear_id"]
-  distance = activity["distance"]
-  if bike_id in bike_distance:
-    bike_distance[bike_id] += distance
-  else:
-    bike_distance[bike_id] = 0
+  print(f"\nRetrieved {len(all_activities)} total activities.")
 
-# Print the miles for each bike
-print ""
-print "Mileage for " + str(year)
-print ""
-for bike_id in bike_distance.keys():
-  bike_distance_miles = round(bike_distance[bike_id] / 1609.344,1)
-  if bike_id in bike_name:
-    justified_bike_name = bike_name[bike_id].ljust(longest_bike_name)
-  else:
-    justified_bike_name = "Unknown".ljust(longest_bike_name)
-  print justified_bike_name, ":",  bike_distance_miles
-print ""
+  # Build a lookup table that maps bike IDs to bike names
+  longest_bike_name = 0
+  bike_name = {}
+  for bike in athlete["bikes"]:
+    bike_name[bike["id"]] = bike["name"]
+    if len(bike["name"]) > longest_bike_name:
+      longest_bike_name = len(bike["name"])
 
+  # Count the number of miles traveled on each bike
+  bike_distance = {}
+  for activity in all_activities:
+    bike_id = activity["gear_id"]
+    distance = activity["distance"]
+    if bike_id in bike_distance:
+      bike_distance[bike_id] += distance
+    else:
+      bike_distance[bike_id] = distance
+
+  # Print the miles for each bike
+  print(f"\nMileage for {year}\n")
+  for bike_id in bike_distance.keys():
+    bike_distance_miles = round(bike_distance[bike_id] / 1609.344,1)
+    if bike_id in bike_name:
+      justified_bike_name = bike_name[bike_id].ljust(longest_bike_name)
+    else:
+      justified_bike_name = "Unknown".ljust(longest_bike_name)
+    print(f"{justified_bike_name} : {bike_distance_miles}")
+  print("")
+
+if __name__ == "__main__":
+  main()
